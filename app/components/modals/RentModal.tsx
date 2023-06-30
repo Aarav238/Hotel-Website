48844a6fdc27ef78a80588b80426f3e5a0420171
Aarav238 <aarav8090shukla@gmail.com>
2023-06-30 11:32:09 +0530

    price input step

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -222,7 +222,28 @@ const RentModal = () => {
     )
   }
 
-  
+  if (steps === STEPS.PRICE){
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+        title="Now, set your price"
+        subtitle="How much do you charge per night"
+        />
+        <Input 
+        id="price"
+        label="Price"
+        formatPrice
+        type="'number"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
+
+        />
+
+      </div>
+    )
+  }
 
   
   return (
