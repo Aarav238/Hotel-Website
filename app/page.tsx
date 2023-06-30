5f0977b486ac092e2a3e5930f90f98458f2d670a
Aarav238 <aarav8090shukla@gmail.com>
2023-06-30 19:31:15 +0530

    empty state component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ClientOnly from "./components/navbar/ClientOnly";
+
 export default function Home() {
-  return <div className="text-rose-500 text-2xl">hello world</div>;
+  const isEmpty = true;
+  if (isEmpty) {
+    return (
+      <ClientOnly>
+        <EmptyState />
+      </ClientOnly>
+    )
+  }
+
+  return (
+    <ClientOnly>
+      <Container>
+        <div
+        className="
+        pt-24
+        grid
+        grid-cols-1
+        sm:grid-cols-2
+        md:grid-cols-3
+        lg:grid-cols-4
+        xl:grid-cols-5
+        2xl:grid-cols-6
+        gap-8
+        "
+        >
+
+      <div> My Future Listing</div>
+
+        </div>
+      </Container>
+    </ClientOnly>
+  )
 }
